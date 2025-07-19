064de9353bc7bef5edd4b1121e371ac8992f37c8
tosaken1116 <tosaken.dev@gmail.com>
2025-07-20 02:28:41 +0900

    feat: enhance import path generation for services in Connect RPC client

--- tools/protoc-gen-connectrpc-client/main.go
+++ tools/protoc-gen-connectrpc-client/main.go
@@ -45,7 +45,9 @@ func generateFile(gen *protogen.Plugin, file *protogen.File) error {
 		g := gen.NewGeneratedFile(filename, file.GoImportPath)
 
 
-		g.P("import { ", serviceName, " } from \"@/generated/", serviceName,serviceName,"_pb", "\";")
+		importPath := getImportPath(domain, serviceName)
+		
+		g.P("import { ", serviceName, " } from \"@/generated/", importPath, "\";")
 		g.P("import { createClient } from \"@connectrpc/connect\";")
 		g.P("import { createConnectTransport } from \"@connectrpc/connect-web\";")
 		g.P("")
@@ -60,4 +62,22 @@ func generateFile(gen *protogen.Plugin, file *protogen.File) error {
 func getPackagePath(file *protogen.File) string {
 	pkg := string(file.Desc.Package())
 	return strings.ReplaceAll(pkg, ".", "/") + "_connect"
+}
+
+func getImportPath(domain, serviceName string) string {
+	switch serviceName {
+	case "UserService":
+		return domain + "/user_pb"
+	case "StringItemService":
+		if domain == "cards" {
+			return domain + "/card_pb"
+		}
+		return domain + "/string_pb"
+	}
+	
+	serviceFile := strings.ToLower(serviceName)
+	if strings.HasSuffix(serviceFile, "service") {
+		serviceFile = serviceFile[:len(serviceFile)-7]
+	}
+	return domain + "/" + serviceFile + "_pb"
 }
